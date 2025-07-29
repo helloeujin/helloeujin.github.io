f4d5081603873131b639da1783a18bbe790159ae
helloeujin <meggonagul@gmail.com>
2025-07-29 19:33:38 +0900

    align adjusted

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -18,16 +18,16 @@ const Portfolio = () => {
 
   const data = require("./data/data.json");
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setClickedIndex((state) => ({
-        ...state,
-        0: !state[0],
-      }));
-    }, 2400);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setClickedIndex((state) => ({
+  //       ...state,
+  //       0: !state[0],
+  //     }));
+  //   }, 2400);
 
-    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
-  }, []);
+  //   return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+  // }, []);
 
   return (
     <div className="portfolio">
