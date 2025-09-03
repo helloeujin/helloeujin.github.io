2a88e4699f5694261f11847750e5beda525d3803
helloeujin <meggonagul@gmail.com>
2025-09-03 12:15:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -18,32 +18,9 @@ function App() {
   return (
     <>
       <div className="intro-text">
-        Hello! I'm <span class="name">Youjin Shin</span>.
-        {/* <br /> I design, code and write for data visualization ✨ */}
-        {/* <br /> I love data visualization ✨ */}
-        <br /> I{" "}
-        {/* <RoughNotation
-          type="underline"
-          color="rgba(0, 0, 255, 0.4)"
-          strokeWidth="3.2"
-          show={true}
-          animationDuration="1200"
-          padding="130"
-        > */}
-        design and code
-        {/* </RoughNotation>{" "} */}
-        with{" "}
-        <div class="intro-data">
-          {/* <RoughNotation
-            type={"highlight"}
-            color="rgba(0, 0, 255, .48)"
-            strokeWidth="3.2"
-            show={true}
-            animationDuration="1200"
-          > */}
-          data
-          {/* </RoughNotation> */}
-        </div>
+        {/* Hello! I'm <span class="name">Youjin Shin</span>.
+        <br /> I design and code with data. */}
+        <span class="name">Youjin Shin</span>, <br />I design and code with data
       </div>
 
       <Portfolio />
